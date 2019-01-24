76d67e7e8e900575999cc1a8e829e802919c8453
Andrey Leontev <leontev.andrey.s@gmail.com>
2019-01-24 23:32:19 +1000

    Add select and data to sql script

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -2,4 +2,12 @@ class EmployeeController < ApplicationController
   def index
     @employees = Employee.top_level
   end
+
+  def show 
+    if (params['employee_id'] == "")
+      return redirect_to '/'
+    end
+    @user = Employee.find(params['employee_id'].to_i)
+    @user
+  end
 end
