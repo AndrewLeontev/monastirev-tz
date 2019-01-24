8ba4c88a0f771e21698ee6657302b626323b270b
Andrey Leontev <leontev.andrey.s@gmail.com>
2019-01-25 01:00:15 +1000

    Fix bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,17 +17,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-$(document).ready(function() {
-    $(".ul-drop").find("li:has(ul)").prepend('<div class="drop"></div>');
-	$(".ul-drop div.drop").click(function() {
-		if ($(this).nextAll("ul").css('display')=='none') {
-			$(this).nextAll("ul").slideDown(400);
-			$(this).css({'background-position':"-11px 0"});
-		} else {
-			$(this).nextAll("ul").slideUp(400);
-			$(this).css({'background-position':"0 0"});
-		}
-	});
-	$(".ul-drop").find("ul").slideUp(400).parents("li").children("div.drop").css({'background-position':"0 0"});
-});
