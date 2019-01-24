57d1c21a73866f112d1979da05fb636f39bc601a
Andrey Leontev <leontev.andrey.s@gmail.com>
2019-01-25 00:02:44 +1000

    Add expand all btn

--- app/assets/javascripts/droptree.js
+++ app/assets/javascripts/droptree.js
@@ -11,4 +11,11 @@ $(document).ready(function() {
 		}
 	});
 	$(".ul-drop").find("ul").slideUp(400).parents("li").children("div.drop").css({'background-position':"0 0"});
+
+	$(".lasttreedrophide").click(function(){
+		$("#tree").find("ul").slideUp(400).parents("li").children("div.drop").css({'background-position':"0 0"});
+	});
+    $(".lasttreedropshow").click(function(){
+		$("#tree").find("ul").slideDown(400).parents("li").children("div.drop").css({'background-position':"-11px 0"});
+	});
 });
